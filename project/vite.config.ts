0524729a04b3063cc9fd08ea0c28ec8f452fb0ed
Pe-hu-b <Construct2@ukr.net>
2026-02-23 18:31:45 +0200

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PortfolioEkrots/',
+  base: '/project-bolt-sb1-fuwlwpde/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
